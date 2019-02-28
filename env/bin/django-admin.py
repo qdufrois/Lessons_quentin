72139e162dcaec3857c39a5a378b431cdf97c565
qdufrois <quentin.dufrois@gmail.com>
2019-02-28 16:35:40 +0100

    Creating venv and django project

---
+++ env/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/quentin/Documents/Agility factory/Cours_Hugo/Lessons/env/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
